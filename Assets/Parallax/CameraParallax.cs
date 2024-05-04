75cc1371743684ff850fd7aa340ba8dc8f0fd3e2
Noperi0r <mathewgwon@gmail.com>
2024-05-04 17:47:24 +0900

    CamParallax code created
    1,2 -> YH
    3,4,5 -> BH

---
+++ Assets/Parallax/CameraParallax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraParallax : MonoBehaviour
+{
+    // Component for getting eye transformation 
+
+    void Update()
+    {
+        
+    }
+
+    // Get Monitor Information Method
+
+    // Compute REsc Method
+
+    // ---------
+
+    // Compute VEsc method
+
+    // Compute Tvector method
+
+    // Compute Translation DF method
+}
