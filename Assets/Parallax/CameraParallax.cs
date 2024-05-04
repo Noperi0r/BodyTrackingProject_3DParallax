8163138c91a7b51335775f32c2b891e1078dab86
Noperi0r <mathewgwon@gmail.com>
2024-05-05 03:13:26 +0900

    feature - Getting real screen width height size, Eye position in screen coordinate
    TestScene에서 테스트 가능

--- Assets/Parallax/CameraParallax.cs
+++ Assets/Parallax/CameraParallax.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class CameraParallax : MonoBehaviour
 {
-    // Component for getting eye transformation 
+    Transform eyeTrackedTransform; // Sensor-tracked eye transform
+    Transform rEyeTransformSC; 
+
+    DisplayInfo mainDisplayInfo;
+    float rScreenWidth, rScreenHeight; // Real screen size in meter
+    const float const_InchToMeter = 0.0254f;
+    
+    void Start()
+    {
+        mainDisplayInfo = Screen.mainWindowDisplayInfo;
+
+        GetScreenSize_InMeter();
+    }
 
     void Update()
     {
         
     }
 
-    // Get Monitor Information Method
+    void GetScreenSize_InMeter()
+    {
+        rScreenWidth = (mainDisplayInfo.width / Screen.dpi) * const_InchToMeter; 
+        rScreenHeight = (mainDisplayInfo.height / Screen.dpi) * const_InchToMeter;
+        Debug.Log(rScreenWidth + " / " + rScreenHeight); // TODO: Slightly bigger than the actual value
+        // 0.6773334 / 0.381
+    }
 
-    // Compute REsc Method
+    void GetRealEyePos_InScreenCoord()
+    {
+        Vector3 rEyePosWC = new Vector3();
+        rEyePosWC = eyeTrackedTransform.position + new Vector3(rScreenHeight/2, rScreenHeight/2, rScreenHeight/2);
+
+        rEyeTransformSC.position = rEyePosWC / rScreenWidth;
+    }
 
     // ---------
 
